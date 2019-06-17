04797c795201d5ca5cbb23407b17cc7355d1ad08
VanHaoChuan <32430258+VanHaoChuan@users.noreply.github.com>
2019-06-18 01:12:23 +0800

    All uploaded Mother fu*ker
    its fooking this.

---
+++ SocketChatCSharpVer/Client/Client.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+namespace SocketChatCSharpVersion
+{
+    public class Client
+    {
+        Socket clientSocket;
+        EndPoint targetEndPoint;
+        static byte[] messageByte = new byte[1024];
+
+        public Client(Socket serverSocket)
+        {
+            clientSocket = new Socket(serverSocket.AddressFamily, serverSocket.SocketType, serverSocket.ProtocolType);
+            targetEndPoint = serverSocket.LocalEndPoint;
+        }
+        public Client(SocketType socketType,string iP,int port)
+        {
+            clientSocket = new Socket(AddressFamily.InterNetwork,socketType,ProtocolType.Tcp);
+            IPAddress targetIPAddress = IPAddress.Parse(iP);
+            targetEndPoint = new IPEndPoint(targetIPAddress,port);
+        }
+        void InitClient()
+        {
+            clientSocket.Connect(targetEndPoint);
+        }
+        void SendMessageToServer(string message)
+        {
+            clientSocket.Send(Encoding.UTF8.GetBytes(message));
+        }
+        string ReceiveMessageFromServer()
+        {
+            int messageLength = clientSocket.Receive(messageByte);
+            return Encoding.UTF8.GetString(messageByte, 0, messageLength);
+        }
+        static void ThreadAwakeCheck(Thread targetThread)
+        {
+            if(targetThread.ThreadState == ThreadState.Stopped)
+            {
+                Console.WriteLine(targetThread.ThreadState);
+                targetThread.Start();
+            }
+        }
+    
+        public static void Main(String[] args)
+        {
+            Client client = new Client(SocketType.Stream,"127.0.0.1",7788);
+            client.InitClient();
+            //Thread sendingThread = new Thread(()=>{client.SendMessageToServer(Console.ReadLine());Thread.Sleep(500);});
+            //sendingThread.Start();
+            //Thread receivingThread = new Thread(()=>{Console.WriteLine(client.ReceiveMessageFromServer());Thread.Sleep(200);messageByte = new Byte[1024];});
+            //receivingThread.Start();
+            System.Threading.Timer threadCheckingTimer = new System.Threading.Timer((object state)=>{
+                //Console.WriteLine("Recieving:"+receivingThread.ThreadState+"\n"+"Sending:"+sendingThread.ThreadState+"\n"+"Server connecting state:"+client.clientSocket.Connected+"\n"+"Byte message:"+Encoding.UTF8.GetString(messageByte)+"\n");
+                //ThreadAwakeCheck(receivingThread);
+                client.SendMessageToServer(Console.ReadLine());
+                Console.WriteLine(client.ReceiveMessageFromServer());
+                },"",200,100);
+                while(true);
+        }
+    }
+}
---
+++ SocketChatCSharpVer/Server/Server.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+namespace SocketChatCSharpVersion
+{
+    public class Server
+    {
+        
+        public Socket serverSocket;
+        Socket clientSocket;
+        IPAddress targetIP;
+        EndPoint targetEndPoint;
+        public bool debugMode = false;
+        static byte[] messageByte = new byte[1024];
+
+        public Server(SocketType streamType,string iP,int port)
+        {
+            serverSocket = new Socket(AddressFamily.InterNetwork, streamType, ProtocolType.Tcp);
+            targetIP = IPAddress.Parse(ipString:iP);
+            targetEndPoint = new IPEndPoint(address:targetIP,port:port);
+
+        }
+        public void InitServer(int maxLinkingCount)
+        {
+            serverSocket.Bind(localEP:targetEndPoint);
+            serverSocket.Listen(backlog: maxLinkingCount);
+            if(debugMode)
+            {
+                Console.WriteLine("Waiting for client socket connecting...");
+            }
+            clientSocket = serverSocket.Accept();
+            if(debugMode)
+            {
+                Console.WriteLine("Client socket connected...");
+            }
+        }
+        public void SendMessageToClientServer(string message)
+        {
+            clientSocket.Send(Encoding.UTF8.GetBytes(message));
+            if(debugMode)
+            {
+                Console.WriteLine(message + " " + "has been sended to the client socket...");
+            }
+        }
+        public string ReceiveMessageFromClient()
+        {
+            int messageLength = clientSocket.Receive(messageByte);
+            string receiveMessage = Encoding.UTF8.GetString(messageByte, 0, messageLength);
+            if(debugMode)
+            {
+                Console.WriteLine(receiveMessage + " " + "has been gotten from the client socket");
+            }
+            return receiveMessage;
+        }
+        public static void Main(String[] args)
+        {
+            Server server = new Server(SocketType.Stream,"127.0.0.1",7788);
+            server.InitServer(100);
+            server.debugMode = false;
+            //Thread receivingThread = new Thread(()=>{Console.WriteLine(server.ReceiveMessageFromClient());Thread.Sleep(200);});
+            //receivingThread.Start();
+            //Thread sendingThread = new Thread(()=>{server.SendMessageToClientServer(Console.ReadLine());Thread.Sleep(500);});
+            //sendingThread.Start();
+            Timer serverTimer = new Timer((object state)=>{
+                Console.WriteLine(server.ReceiveMessageFromClient());
+                server.SendMessageToClientServer(Console.ReadLine());
+            },"",200,100);
+            while (true);
+        }
+    }
+}
